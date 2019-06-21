f4cb2acf2233f26f0980d223b9ad71b0e475b975
Haley DeKeyser <hcd09@live.com>
2019-06-21 20:12:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,12 @@ class App < Sinatra::Base
     "My name is "
   end
   
+  get '/hometown' do
+    "My hometown is "
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is "
+  end
+  
 end
